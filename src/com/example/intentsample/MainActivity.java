23a2806adb73533c068591db23f1a7366f9dbb13
samhithakrish <vedantamskc@gmail.com>
2015-03-23 22:16:15 -0700

    First Commit

---
+++ src/com/example/intentsample/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.intentsample;
+
+import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends ActionBarActivity {
+	
+	Button btnOpen;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		btnOpen = (Button)findViewById(R.id.btnOpen);
+		btnOpen.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent i = new Intent(Intent.ACTION_SEND);
+				i.setType("text/plain");
+				i.putExtra(Intent.EXTRA_SUBJECT, "Test");
+				i.putExtra(Intent.EXTRA_EMAIL, new String[]{"krish9nas@gmail.com"});
+				i.putExtra(Intent.EXTRA_SUBJECT,"Test Sub");
+				i.putExtra(Intent.EXTRA_TEXT, "This is test mail");
+				startActivity(i);//Intent.createChooser(i, "Choose an email client")));
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
